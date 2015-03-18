0a15e7e7646d79dd1c92a97ea71f2cd9ed734424
irrelon <rob@irrelon.com>
2015-03-18 15:53:41 +0000

    1.0.2 - added browserify, see changelog for others

--- Emitter.js
+++ Emitter.js
@@ -22,9 +22,15 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 
- Source: https://github.com/coolbloke1324/emitter
+ Source: https://github.com/irrelon/emitter
 
  Changelog:
+ 	Version 1.0.2:
+ 		Removed AMD support, added browserify support
+ 		Added package.json
+ 		Added once() method
+ 		Added hasListener() method
+ 		Published to NPM as irrelon-emitter
  	Version 1.0.1:
  		Added ability to extend any object with eventing capability
  		Added AMD / Require.js support
@@ -36,29 +42,42 @@ var Emitter = (function () {
 	var Emitter = function (obj) {
 		if (obj) {
 			// Convert the object prototype to have eventing capability
-			obj.prototype.on = this.prototype.on;
-			obj.prototype.off = this.prototype.off;
-			obj.prototype.emit = this.prototype.emit;
+			obj.prototype.on = Emitter.prototype.on;
+			obj.prototype.off = Emitter.prototype.off;
+			obj.prototype.once = Emitter.prototype.once;
+			obj.prototype.emit = Emitter.prototype.emit;
+			obj.prototype.hasListener = Emitter.prototype.hasListener;
 		}
 	};
 
 	Emitter.prototype.on = function(event, listener) {
 		this._listeners = this._listeners || {};
 		this._listeners[event] = this._listeners[event] || [];
-		this._listeners[event].push(listener);
+		this._listeners[event].push({once: false, listener: listener});
 	};
 
 	Emitter.prototype.off = function(event, listener) {
 		if (this._listeners) {
 			if (event in this._listeners) {
 				var arr = this._listeners[event],
-					index = arr.indexOf(listener);
+					arrCount = arr.length,
+					arrIndex;
 
-				if (index > -1) {
-					arr.splice(index, 1);
+				for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
+					if (arr[arrIndex].listener === listener) {
+						arr.splice(arrIndex, 1);
+						break;
+					}
+
 				}
 			}
 		}
+	};
+
+	Emitter.prototype.once = function(event, listener) {
+		this._listeners = this._listeners || {};
+		this._listeners[event] = this._listeners[event] || [];
+		this._listeners[event].push({once: true, listener: listener});
 	};
 
 	Emitter.prototype.emit = function(event, data) {
@@ -67,28 +86,29 @@ var Emitter = (function () {
 		if (event in this._listeners) {
 			var arr = this._listeners[event],
 				arrCount = arr.length,
-				arrIndex;
+				arrIndex,
+				removeArr = [];
 
 			for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
-				arr[arrIndex].apply(this, Array.prototype.slice.call(arguments, 1));
+				arr[arrIndex].listener.apply(this, Array.prototype.slice.call(arguments, 1));
+				if (arr[arrIndex].once) {
+					removeArr.push(arrIndex);
+				}
+			}
+
+			arrCount = removeArr.length;
+
+			for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
+				arr.splice(removeArr[arrIndex], 1);
 			}
 		}
 	};
+
+	Emitter.prototype.hasListener = function (event) {
+		return this._listeners && event in this._listeners && this._listeners[event].length;
+	};
+
+	return Emitter;
 })();
 
-/**
- * Node.js module support.
- */
-if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') {
-	Emitter.prototype._isServer = true;
-	module.exports = Emitter;
-}
-
-/**
- * AMD module support.
- */
-if (typeof(define) === 'function' && define.amd) {
-	define([], function() {
-		return Emitter;
-	});
-}
+module.exports = Emitter;
